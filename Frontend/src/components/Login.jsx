bc6d23cd44b8db7a344b5f5ebc1ac46a4be1ad7f
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-17 20:57:25 +0530

    Update

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = forwardRef((props, ref) => {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://bookie-server-8w6xythog-shivah12s-projects.vercel.app/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
